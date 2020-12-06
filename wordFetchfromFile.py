343f02f74e63d8037e4cb86190d1ac9338db96f0
vij <60034355+vijvalp@users.noreply.github.com>
2020-12-06 21:22:25 +0000

    Delete wordFetchfromFile.py

--- wordFetchfromFile.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def wordFetch():
-    with open("wordlist.txt") as word_file:
-        words = word_file.read().split()
-        word = random.choice(words)
-        return word
-
-print(wordFetch())
