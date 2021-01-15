d01c8abfe0d63e17b575a1a873802bb8df22c364
StreakSharn <74095821+StreakSharn@users.noreply.github.com>
2021-01-15 21:22:47 +0000

    difficulty inclusion

--- main.py
+++ main.py
@@ -6,14 +6,37 @@ with open('wordlist.txt','r') as f:
     wordList = f.read()
 wordList = wordList.split('\n')
 
-
-def searchWords(word):
+def searchWords(word):  #function to select words of a particular length
     if len(word) == length:
         return word
 
-length = int(input('How long do you want your word to be? \t'))
 
-words = (map(searchWords,wordList)) #find words of correct length
-words = list(filter(None,words))    #get rid of wrong answers
+#option select
+difficulty = input('What difficulty do you want to play on? Easy, medium, or hard?\n')
+
+game = 'GO'
+while game == 'GO':
+    if difficulty.lower() == 'easy':    #create given list
+        words = wordlist = ['apple', 'banana', 'carriage', 'dog', 'earth', 'watermelon', 'pi', 'dinosaur', 'coconut', 'sunflower', 'dawn', 'despair', 'lightning', 'northwest', 'cruise', 'mission', 'tarantula', 'fortress']
+
+    elif difficulty.lower() == 'medium':    #transfer file
+        words = wordList
+
+    elif difficulty.lower() == 'hard':  #select specific lengths from file
+        length = int(input('How long do you want your word to be? \t'))
+
+        words = (map(searchWords,wordList)) #find words of correct length
+        words = list(filter(None,words))    #get rid of wrong answers
+
+        if len(words) == 0:
+            print('No words of that length were found.\n')
+            continue
+    else:
+        difficulty = input("That's not a difficulty - type easy, medium, or hard.")
+        continue
+
+    game = 'STOP'
+
+
 
 print(choice(words).upper())    #output word
